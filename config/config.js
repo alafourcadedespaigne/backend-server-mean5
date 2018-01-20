6fcd4f6e2029792a622fc8d46364374311dde713
Alejandro Lafourcade Despaigne <alafourcadedespaigne@gmail.com>
2018-01-20 03:15:03 -0300

    Finalizado el backend

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+// Google
+module.exports.GOOGLE_CLIENT_ID = '477364803157-ld90585c8ncdppapggqpanuda8u45qju.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'kRobEWFGbS33gtamaPyvMR8v';
--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,135 @@
-var express = require('express');
-var bcrypt = require('bcryptjs');
-var jwt = require('jsonwebtoken');
+let express = require('express');
+let bcrypt = require('bcryptjs');
+let jwt = require('jsonwebtoken');
 
-var SEED = require('../config/config').SEED;
+let SEED = require('../config/config').SEED;
 
-var app = express();
-var Usuario = require('../models/usuario');
+let app = express();
+let Usuario = require('../models/usuario');
 
+
+let GoogleAuth = require('google-auth-library');
+let auth = new GoogleAuth;
+
+const GOOGLE_CLIENT_ID = require('../config/config').GOOGLE_CLIENT_ID;
+const GOOGLE_SECRET = require('../config/config').GOOGLE_SECRET;
+
+// ==========================================
+//  Autenticación De Google
+// ==========================================
+app.post('/google', (req, res) => {
+
+    let token = req.body.token || 'XXX';
+
+
+    let client = new auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_SECRET, '');
+
+    client.verifyIdToken(
+        token,
+        GOOGLE_CLIENT_ID,
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3],
+        function(e, login) {
+
+            if (e) {
+                return res.status(400).json({
+                    ok: true,
+                    mensaje: 'Token no válido',
+                    errors: token
+                });
+            }
+
+
+            let payload = login.getPayload();
+            let userid = payload['sub'];
+            // If request specified a G Suite domain:
+            //let domain = payload['hd'];
+
+            Usuario.findOne({ email: payload.email }, (err, usuario) => {
+
+                if (err) {
+                    return res.status(500).json({
+                        ok: true,
+                        mensaje: 'Error al buscar usuario - login',
+                        errors: err
+                    });
+                }
+
+                if (usuario) {
+
+                    if (usuario.google === false) {
+                        return res.status(400).json({
+                            ok: true,
+                            mensaje: 'Debe de usar su autenticación normal'
+                        });
+                    } else {
+
+                        usuario.password = ':)';
+
+                        let token = jwt.sign({ usuario: usuario }, SEED, { expiresIn: 14400 }); // 4 horas
+
+                        res.status(200).json({
+                            ok: true,
+                            usuario: usuario,
+                            token: token,
+                            id: usuario._id
+                        });
+
+                    }
+
+                    // Si el usuario no existe por correo
+                } else {
+
+                    let usuario = new Usuario();
+
+
+                    usuario.nombre = payload.name;
+                    usuario.email = payload.email;
+                    usuario.password = ':)';
+                    usuario.img = payload.picture;
+                    usuario.google = true;
+
+                    usuario.save((err, usuarioDB) => {
+
+                        if (err) {
+                            return res.status(500).json({
+                                ok: true,
+                                mensaje: 'Error al crear usuario - google',
+                                errors: err
+                            });
+                        }
+
+
+                        let token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
+
+                        res.status(200).json({
+                            ok: true,
+                            usuario: usuarioDB,
+                            token: token,
+                            id: usuarioDB._id
+                        });
+
+                    });
+
+                }
+
+
+            });
+
+
+        });
+
+
+
+
+});
+
+// ==========================================
+//  Autenticación normal
+// ==========================================
 app.post('/', (req, res) => {
 
-    var body = req.body;
+    let body = req.body;
 
     Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
 
@@ -40,7 +160,7 @@ app.post('/', (req, res) => {
         // Crear un token!!!
         usuarioDB.password = ':)';
 
-        var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
+        let token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
 
         res.status(200).json({
             ok: true,
